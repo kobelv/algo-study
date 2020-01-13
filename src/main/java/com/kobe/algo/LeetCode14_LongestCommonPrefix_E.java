6277e96a7af1b56e4f4d1b2d7da1b237aaf53ca6
kobelv <lvhangfei@outlook.com>
2020-01-13 21:38:25 +0800

    add more leetcode

---
+++ src/main/java/com/kobe/algo/LeetCode14_LongestCommonPrefix_E.java
@@ -0,0 +1,20 @@
+package com.kobe.algo;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1:
+ *
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ *
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ *
+ */
+public class LeetCode14_LongestCommonPrefix_E {
+}
